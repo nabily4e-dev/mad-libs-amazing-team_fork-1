c293f01a1d0df3b6723ce2425da92b3f60908f9c
A-Abd <abrar.abdulwahed@gmail.com>
2023-02-01 17:46:58 +0300

    chore: show other contents according to lang

--- locale.js
+++ locale.js
@@ -1,2 +1,23 @@
-const locale = "en"
+const locale = "en";
+let translations = {};
+document.addEventListener("DOMContentLoaded", () => {
+  setLocale(locale);
+});
+async function setLocale() {
+  response = await fetch(`/lang/${locale}/details.json`);
+  translations = await response.json();
+  translatePage();
+}
+
+function translatePage() {
+  document
+    .querySelectorAll("[data-key]")
+    .forEach(elem => {
+        const key = elem.getAttribute("data-key");
+        const translation = translations[key];
+        elem.innerText = translation;
+    });
+}
+
+
 
